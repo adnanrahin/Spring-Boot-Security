cc0e0b5d052607164826be2f7d260b73bdb2b81b
Adnan Rahin <adnan.rahin13@gmail.com>
2021-02-09 02:11:27 -0500

    Basic Auth Using Configure

--- basic-auth-sixty-four/src/main/java/com/spring/boot/basicauth/model/Student.java
+++ basic-auth-sixty-four/src/main/java/com/spring/boot/basicauth/model/Student.java
@@ -48,4 +48,14 @@ public class Student {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
--- basic-auth-sixty-four/src/main/java/com/spring/boot/basicauth/security/ApplicationSecurityConfig.java
+++ basic-auth-sixty-four/src/main/java/com/spring/boot/basicauth/security/ApplicationSecurityConfig.java
@@ -3,6 +3,8 @@ package com.spring.boot.basicauth.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,10 +14,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.spring.boot.basicauth.model.ApplicationUserPermission.*;
 import static com.spring.boot.basicauth.model.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
@@ -25,42 +29,49 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         this.passwordEncoder = passwordEncoder;
     }
 
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers("/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
     }
 
-
     @Override
     @Bean
     protected UserDetailsService userDetailsService() {
-        UserDetails student = User.builder()
+        UserDetails annaSmithUser = User.builder()
                 .username("student")
                 .password(passwordEncoder.encode("12345"))
-                .roles(STUDENT.name())
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
-        UserDetails admin = User.builder()
+        UserDetails lindaUser = User.builder()
                 .username("admin")
                 .password(passwordEncoder.encode("12345"))
-                .roles(ADMIN.name())
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
-        UserDetails internAdmin = User.builder()
+        UserDetails tomUser = User.builder()
                 .username("intern")
                 .password(passwordEncoder.encode("12345"))
-                .roles(ADMININTERN.name())
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
         return new InMemoryUserDetailsManager(
-                student, admin, internAdmin
+                annaSmithUser,
+                lindaUser,
+                tomUser
         );
+
     }
 }
