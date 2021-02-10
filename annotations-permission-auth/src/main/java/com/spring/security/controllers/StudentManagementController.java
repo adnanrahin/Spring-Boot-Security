2b98c76807cf7dd56a0b3e714aa3b20591a5a40a
Adnan Rahin <adnan.rahin13@gmail.com>
2021-02-09 19:55:25 -0500

    Annotations Based Access

---
+++ annotations-permission-auth/src/main/java/com/spring/security/controllers/StudentManagementController.java
@@ -0,0 +1,43 @@
+package com.spring.security.controllers;
+
+import com.spring.security.model.Student;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/management/api/")
+public class StudentManagementController {
+
+    private static final List<Student> students = Arrays.asList(
+            new Student(1L, "Megatron", "NLN", "megatron@email.com"),
+            new Student(2L, "Optimus", "Prime", "optimusprime@email.com"),
+            new Student(3L, "Iron", "Hide", "ironhide@email.com"),
+            new Student(4L, "Vector", "Prime", "vectorprime@email.com")
+    );
+
+    @RequestMapping(method = RequestMethod.GET, path = "/getAll")
+    public List<Student> getAllStudents() {
+        System.out.println("getAllStudents");
+        return students;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/save")
+    public void registerNewStudent(@RequestBody Student student) {
+        System.out.println("registerNewStudent");
+        System.out.println(student);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, path = "/delete/{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Integer studentId) {
+        System.out.println("deleteStudent");
+        System.out.println(studentId);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, path = "/update/{studentId}")
+    public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
+        System.out.println("updateStudent");
+        System.out.println(String.format("%s %s", studentId, student));
+    }
+}
---
+++ annotations-permission-auth/src/main/java/com/spring/security/security/ApplicationSecurityConfig.java
@@ -0,0 +1,77 @@
+package com.spring.security.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.spring.security.model.ApplicationUserPermission.COURSE_WRITE;
+import static com.spring.security.model.ApplicationUserRole.*;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers("/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmithUser = User.builder()
+                .username("student")
+                .password(passwordEncoder.encode("12345"))
+                .authorities(STUDENT.getGrantedAuthorities())
+                .build();
+
+        UserDetails lindaUser = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("12345"))
+                .authorities(ADMIN.getGrantedAuthorities())
+                .build();
+
+        UserDetails tomUser = User.builder()
+                .username("intern")
+                .password(passwordEncoder.encode("12345"))
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                annaSmithUser,
+                lindaUser,
+                tomUser
+        );
+
+    }
+}
